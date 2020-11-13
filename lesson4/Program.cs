76ee8c313ba39dcd2d6d32dd0754e3ec0ed964aa
klerik86elvin <62464631+klerik86elvin@users.noreply.github.com>
2020-11-13 12:19:26 +0400

    решение задачи 1

---
+++ lesson4/Program.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = createArray();
+
+            int count = 0;
+
+            for(int i = 0; i < arr.Length-1; i++)
+            {                
+                if(isValid(arr[i]) || isValid(arr[i + 1]))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+            
+        }
+
+        public static int[] createArray()
+        {
+            int[] arr = new int[20];
+            Random random = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int val = random.Next(-10000, 10000);
+                arr[i] = val;
+            }
+
+            return arr;
+        }
+
+
+        //создает массив из 20ти элементов
+        public static void print(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+
+        //проверяет на деление на 3
+        public static bool isValid(int i)
+        {
+            return i % 3 == 0;
+        }
+    }
+}
